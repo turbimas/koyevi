d38003a3330e22bac554c4ea0bb76a333cf837c4
Turbim A.Ş <115806503+turbimas@users.noreply.github.com>
2022-11-26 10:02:32 -0800

    app_api_change

--- lib/product/constants/app_constants.dart
+++ lib/product/constants/app_constants.dart
@@ -15,7 +15,7 @@ class AppConstants {
   static const IOS_Version = "1.0.0";
   static const ANDROID_Version = "1.0.0";
 
-  static const APP_API = "http://api.goldenerp.com/api/";
+  static const APP_API = "https://api.koyevidogal.com:444/api/";
 
   static String APP_TOKEN = "";
 }
